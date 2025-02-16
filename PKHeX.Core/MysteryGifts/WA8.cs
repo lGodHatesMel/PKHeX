5d78910817f37cc60cc1f7fca247487f3ef4ede4
Buggz · ฅ^•ﻌ•^ฅ <56521233+lGodHatesMel@users.noreply.github.com>
2025-02-15 20:12:12 -0500

    Some Changes for Home Gift for WA8 and WB8

--- PKHeX.Core/MysteryGifts/WA8.cs
+++ PKHeX.Core/MysteryGifts/WA8.cs
@@ -510,8 +510,8 @@ public sealed class WA8(byte[] Data) : DataMysteryGift(Data), ILangNick, INature
             SetEggMetData(pk);
         pk.CurrentFriendship = pk.IsEgg ? pi.HatchCycles : pi.BaseFriendship;
 
-        pk.HeightScalar = PokeSizeUtil.GetRandomScalar(rnd);
-        pk.WeightScalar = PokeSizeUtil.GetRandomScalar(rnd);
+        pk.HeightScalar = GetScalar(rnd);
+        pk.WeightScalar = GetScalar(rnd);
         pk.Scale = pk.HeightScalar;
         pk.ResetHeight();
         pk.ResetWeight();
@@ -622,6 +622,14 @@ public sealed class WA8(byte[] Data) : DataMysteryGift(Data), ILangNick, INature
         pk.SetIVs(finalIVs);
     }
 
+    private byte GetScalar(Random rnd)
+    {
+        if (CardID is 9027) // HOME Enamorus is a special case where height/weight is fixed.
+            return 127;
+
+        return PokeSizeUtil.GetRandomScalar(rnd);
+    }
+
     public override bool IsMatchExact(PKM pk, EvoCriteria evo)
     {
         if (!IsEgg)
@@ -678,7 +686,7 @@ public sealed class WA8(byte[] Data) : DataMysteryGift(Data), ILangNick, INature
 
         const int poke = (int)Core.Ball.LAPoke;
         var expectedBall = Ball == 0 ? poke : Ball;
-        if (expectedBall < poke) // Not even Cherish balls are safe! They get set to the proto-Poké ball.
+        if (expectedBall < poke && !IsHOMEGift) // Not even Cherish balls are safe! They get set to the proto-Poké ball. HOME gifts may still use Cherish.
             expectedBall = poke;
         if (pk is PK8)
             expectedBall = (int)Core.Ball.Poke; // Transferred to SW/SH -> Regular Poké ball
@@ -690,6 +698,15 @@ public sealed class WA8(byte[] Data) : DataMysteryGift(Data), ILangNick, INature
 
         if (pk is IGanbaru b && b.IsGanbaruValuesBelow(this))
             return false;
+        
+        // HOME Enamorus has fixed Height/Weight/Scale.
+        if (CardID is 9027)
+        {
+            if (pk is IScaledSize ht && (ht.HeightScalar != 127 || ht.WeightScalar != 127))
+                return false;
+            if (pk is IScaledSize3 s && s.Scale != 127)
+                return false;
+        }
 
         // PID Types 0 and 1 do not use the fixed PID value.
         // Values 2,3 are specific shiny states, and 4 is fixed value.
